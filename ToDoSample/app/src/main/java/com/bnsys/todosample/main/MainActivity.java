8427ff1a7d42fe7572458ffcc4e7c3177405959d
rabbit229 <rabbit229@daum.net>
2019-10-19 15:06:35 +0900

    2st init commit

--- ToDoSample/app/src/main/java/com/bnsys/todosample/main/MainActivity.java
+++ ToDoSample/app/src/main/java/com/bnsys/todosample/main/MainActivity.java
@@ -1,10 +1,14 @@
-package com.bnsys.todosample;
+package com.bnsys.todosample.main;
 
 import android.os.Bundle;
+
+import com.bnsys.todosample.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -18,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
